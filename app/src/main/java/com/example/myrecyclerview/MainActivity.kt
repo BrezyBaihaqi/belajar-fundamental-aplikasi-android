0198418cae7c03c0ca457b35d6f90ea02145d765
Ahmad Briezy Baihaqi <79033889+BrezyBaihaqi@users.noreply.github.com>
2021-02-15 17:36:02 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.dicoding.myrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    // menerapkan Recycler View yg sudah di buat di main_activity.xml
+    private lateinit var rvHeroes: RecyclerView
+    // menginisiasi data yg sudah di buat di HeroesData
+    private var list: ArrayList<Hero> = arrayListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(HeroesData.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
+    }
+
+    // menerapkan main_menu.xml
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when(selectedMode) {
+            R.id.action_list -> {
+
+            }
+            R.id.action_grid -> {
+
+            }
+            R.id.action_cardview -> {
+
+            }
+        }
+    }
+}
